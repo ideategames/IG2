9f50d2bbdb71e5e3e5087c6662b2152de9aa0617
David Marques <david@marques42.org>
2015-05-04 11:22:30 -0700

    Dummy database comm
    Dummy file for communicating usage data to the database.

---
+++ common/js/comm.js
@@ -0,0 +1,11 @@
+//
+// dummy communication file
+//
+
+IGactiveComm = false
+
+function DMMGetHttpRequest(packet,page) {
+// this module communicates to the server database, thus is not made public
+//
+    return
+}
