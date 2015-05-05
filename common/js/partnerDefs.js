25e200fb931a397fbe4ff88c294523b56a87790a
ideategames <david@marques42.org>
2015-05-04 19:43:44 -0700

    Simple Entry for Partners
    Create a simple definition file and easy entry for partner builds.

---
+++ common/js/partnerDefs.js
@@ -0,0 +1,9 @@
+//
+//
+// partnerDefs.js
+//
+// defines the full name of game partners
+//
+Partners = {Museum: 'The British Museum', Alaska: 'the History of Alaska', Facebook: "Facebook", 
+  Elsevier: "Elsevier", Whitman: "Whitman Middle School"
+}
